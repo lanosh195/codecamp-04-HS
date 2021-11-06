a13249d1f3d9cef181633136c57862831e089f9f
lanosh196 <lanosh195@gmail.com>
2021-11-06 23:54:10 +0900

    practice

--- freeboard_frontend/styles/new.js
+++ freeboard_frontend/styles/new.js
@@ -6,13 +6,23 @@ export const Heading = styled.div`
 `
 export const Container = styled.div`
     box-sizing: border-box;
-    width: 100%;
-    height: 1547px;
+    width: 1200px;
+  /* height: 1847px; */
+    border: 1px solid black;
+    margin: 100px;
+    padding-top: 80px;
+    padding-bottom: 100px;
+    padding-left: 102px;
+    padding-right: 102px;
     display: flex;
     flex-direction: column;
     align-items: center;
-       
-    
+    border: none;
+    box-shadow: 0px 0px 10px gray;
+    border: 1px solid black;
+    border: none;
+    box-shadow: 0px 0px 10px gray;
+    border: 1px solid black;
     
 `
 
@@ -23,6 +33,8 @@ export const Wrapper1 = styled.div`
     justify-content: space-between;
     align-items: center;
     padding-top: 10px;
+    
+    
 `
 
 export const Wrappertop= styled.div`
