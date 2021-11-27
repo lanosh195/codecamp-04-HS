5b989bcfcbaa8d23d0713913734943a5a806e69a
lanosh195 <lanosh195@gmail.com>
2021-11-27 21:00:02 +0900

    update

--- class/pages/20-03-search-pagination-Debounce/index.tsx
+++ class/pages/20-03-search-pagination-Debounce/index.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, MouseEvent, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
+import _ from "lodash";
 import {
   IQuery,
   IQueryFetchBoardsArgs,
@@ -15,22 +16,28 @@ const FETCH_BAORDS = gql`
   }
 `;
 
-export default function SearchPaginationPage() {
-  const [mySearch, serMySearch] = useState("");
+export default function SearchPaginationDebouncePage() {
+  // const [mySearch, serMySearch] = useState("");
   const [myKeyword, setMyKeyword] = useState("");
   const { data, refetch } = useQuery<
     Pick<IQuery, "fetchBoards">,
     IQueryFetchBoardsArgs
   >(FETCH_BAORDS);
 
+  const getDebounce = _.debounce((data) => {
+    refetch({ search: data, page: 1 });
+    setMyKeyword(data);
+  }, 500);
+
   function onChangeSearch(event: ChangeEvent<HTMLInputElement>) {
-    serMySearch(event.target.value);
+    getDebounce(event.target.value); ///////<이게 마이서치 역할하게 됨
+    // serMySearch(event.target.value);
   }
-  function onClickSearch() {
-    //mySearch 키워드로 fetchBoard 요청하기!
-    refetch({ search: mySearch, page: 1 }); //쿼리에도 page 추가 해줘야함
-    setMyKeyword(mySearch);
-  }
+  // function onClickSearch() {
+  //   //mySearch 키워드로 fetchBoard 요청하기!
+  //   refetch({ search: mySearch, page: 1 }); //쿼리에도 page 추가 해줘야함
+  //   setMyKeyword(mySearch);
+  // }
 
   function onClickPage(event: MouseEvent<HTMLSpanElement>) {
     if (event.target instanceof Element)
@@ -41,7 +48,7 @@ export default function SearchPaginationPage() {
       <h1>검색 페이지!!!</h1>
       검색어 입력:
       <input type="text" onChange={onChangeSearch} />
-      <button onClick={onClickSearch}>검색</button>
+      {/* <button onClick={onClickSearch}>검색</button> */}
       {data?.fetchBoards.map((el) => (
         <div key={el._id}>
           <span style={{ paddingRight: "50px" }}>{el.writer}</span>
