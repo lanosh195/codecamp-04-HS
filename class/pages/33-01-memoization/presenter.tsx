2604c9f8a14a908bef87e76506569881715898f1
lanosh195 <lanosh195@gmail.com>
2022-03-23 14:39:43 +0900

    0323-update

--- class/pages/33-01-memoization/presenter.tsx
+++ class/pages/33-01-memoization/presenter.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 
 function MemoizationUI(props) {
-  console.log("프레진터가 렌더링 됩니다!!!");
+  console.log("프리젠터가 렌더링 됩니다!!!");
   return (
     <>
       <div>=========================</div>
